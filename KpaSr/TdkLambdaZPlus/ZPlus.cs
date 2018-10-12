bd83f433a7e2ddaf169f3c6ee41c312e807d5473
Егор Гвоздев <gvozdev.e@outlook.com>
2018-10-12 17:48:45 +0300

    1

--- KpaSr/TdkLambdaZPlus/ZPlus.cs
+++ KpaSr/TdkLambdaZPlus/ZPlus.cs
@@ -8,5 +8,17 @@ namespace TdkLambdaZPlus
 {
     public class ZPlus
     {
+        public double I;
+        public double U;
+        public double P;
+        public double Iout;
+        public double Uout;
+        public double Umax;
+        public double Umin;
+        public byte ESR;
+        public byte STB;
+        public string IDN;
+        public bool Output;
+        public bool OutputMode;
     }
 }
